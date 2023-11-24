9efcf56e488e98ed9e92b340ad8c325cb41e6c40
mAJO1012 <carovasquezmariajose@ieperpetuosocorro.edu.co>
2023-11-24 07:48:50 -0500

    todolist edit

--- src/view/components/TodoList/Todolist.jsx
+++ src/view/components/TodoList/Todolist.jsx
@@ -1,41 +1,8 @@
-import { useState, useEffect, useContext } from 'react'
 import { Todoitem } from '../Todoitem/Todoitem'
-import { TaskContext } from '../context/tasks'
 
 export const Todolist = ({ todos, handleUpdateTodo, handleDeleteTodo, handleCompleteTodo }) => {
-  const { dispatch } = useContext(TaskContext)
-  const { name, setName } = useState([])
-  const { description, setDescription } = useState([])
-  const { finishDate, setFinishDate } = useState([])
-
-  useEffect(() => {
-    Todolist()
-  }, [])
-
-  const Todolist = (event) => {
-    fetch('https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/user/auth', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        name,
-        description,
-        finishDate
-      })
-    }).then((data) => {
-      console.log(data)
-      dispatch({ type: 'ADD_TODO', payload: data.user })
-    })
-  }
-
   return (
     <ul>
-      <tr>
-        <th>Nombre</th>
-        <th>Descripción</th>
-        <th>Fecha de finalización</th>
-        <th>Estado</th>
-        <Todoitem />
-      </tr>
       {todos.map(todo => (
         <Todoitem
           key={todo.userId}
@@ -46,11 +13,18 @@ export const Todolist = ({ todos, handleUpdateTodo, handleDeleteTodo, handleComp
         />
       ))}
       <div>
+        <tr>
+          <th>Nombre</th>
+          <th>Descripción</th>
+          <th>Fecha de finalización</th>
+          <th>Estado</th>
+          <Todoitem />
+        </tr>
         <tbody>
           <tr>
-            <td>{setName}</td>
-            <td>{setDescription}</td>
-            <td>{setFinishDate}</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
             <td>.</td>
           </tr>
         </tbody>
