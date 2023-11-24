37a3be6512621d89d87e4444b4f1cb1aeb9fffc6
mAJO1012 <carovasquezmariajose@ieperpetuosocorro.edu.co>
2023-11-24 12:38:41 -0500

    todolist overcome

--- src/view/components/TodoList/Todolist.jsx
+++ src/view/components/TodoList/Todolist.jsx
@@ -1,31 +1,7 @@
+import { ToDo } from '../../pages/ToDo/ToDo'
 import { Todoitem } from '../Todoitem/Todoitem'
 
-export const Todolist = ({ todos, handleUpdateTodo, handleDeleteTodo, handleCompleteTodo, name, description, finishDate, isCompleted }) => {
-  /* useEffect(() => {
-    onReturn()
-  }, [])
-
-  const onReturn = (event) => {
-    const { state, dispatch } = useContext(TaskContext)
-    const [values, setValues] = useState({
-      name: event.target.name,
-      description: event.target.description,
-      finishDate: event.target.finishDate,
-      isCompleted: false,
-      userId: state.user._Id
-    })
-    fetch('https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/todo?userId=', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(values)
-    }).then((response) => {
-      console.log(response)
-      setValues(response.data)
-    }).then((data) => {
-      console.log(data)
-      dispatch({ type: 'ADD_TODO', payload: data.todo })
-    })
-  } */
+export const Todolist = ({ todos, handleUpdateTodo, handleDeleteTodo, handleCompleteTodo, todoList }) => {
   return (
     <ul>
       {todos.map(todo => (
@@ -47,10 +23,11 @@ export const Todolist = ({ todos, handleUpdateTodo, handleDeleteTodo, handleComp
         </tr>
         <tbody>
           <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
+            {
+          todoList && todoList.map(todo => (
+            <ToDo key={todo._id} todo={todoList} />
+          ))
+        }
           </tr>
         </tbody>
       </div>
